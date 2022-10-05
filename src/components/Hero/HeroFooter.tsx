182807f2f80a0980314f6abbbc5997b8475de478
Uche Omodu <chivoomodu@gmail.com>
2022-10-05 18:24:26 +0100

    building Footer

---
+++ src/components/Hero/HeroFooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroFooter = () => {
+  return (
+    <div className="hero-text-section">
+      <h1>Visual stories that feel like yours, because they are.</h1>
+    </div>
+  );
+};
+
+export default HeroFooter;
